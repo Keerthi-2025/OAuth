4da7698105baa166b0281480346c49011a1552d7
Keerthi <rhkeerthi3@gmail.com>
2025-11-26 23:27:13 +0530

    added mapping to logged in user

--- src/main/java/com/example/OAuth/MainController.java
+++ src/main/java/com/example/OAuth/MainController.java
@@ -3,12 +3,19 @@ package com.example.OAuth;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
 public class MainController {
 
 @RequestMapping("/")
     public  String home(){
     return "Welcome Home!";
+}
 
+@RequestMapping("/user")
+    public Principal user(Principal user){
+        return user;
+    }
 }
-}
+
