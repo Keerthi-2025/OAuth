0df52fb1d47ca534f2004c412b1b4e8f6e3c84b2
Keerthi <rhkeerthi3@gmail.com>
2025-11-27 14:05:56 +0530

    modified application properties and main application

--- src/main/java/com/example/OAuth/OAuthProjectApplication.java
+++ src/main/java/com/example/OAuth/OAuthProjectApplication.java
@@ -2,8 +2,10 @@ package com.example.OAuth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class OAuthProjectApplication {
 
 	public static void main(String[] args) {
